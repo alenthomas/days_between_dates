8668977ee7a7875a695624da6f6b15f5ace6d632
Alen Thomas <alenthomas9@gmail.com>
2017-06-15 10:08:03 +0530

    return if years are same; assuming all months are of 30 days

--- dBd.py
+++ dBd.py
@@ -3,6 +3,10 @@ def find(d1, m1, y1, d2, m2, y2, count=0):
         return count
     if (y1 == y2 and m1 == m2):
         return find(d1+1, m1, y1, d2, m2, y2, count+1)
+    if (y1 == y2):
+        days = 30 - d1 + 1
+        d1 = 1
+        return find(d1, m1+1, y1, d2, m2, y2, count + days)
 
 if __name__ == "__main__":
     print(find(5, 1, 2017, 30, 2, 2017))
