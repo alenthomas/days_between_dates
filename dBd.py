554fb97301399bfed46a3c0b3aa0685654089288
Alen Thomas <alenthomas9@gmail.com>
2017-06-15 23:40:15 +0530

    adds timing function; uses calendar to check for leap year

--- dBd.py
+++ dBd.py
@@ -1,15 +1,13 @@
 import calendar
+import time
 
 _leap_months = {1:31, 2:29, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31,
                 9:30, 10:31, 11:30, 12:31}
 _months = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31,
            9:30, 10:31, 11:30, 12:31}
 
-def is_leap_year(year):
-    return calendar.isleap(year)
-
 def days_in_month(month, year):
-    if is_leap_year(year):
+    if calendar.isleap(year):
         return _leap_months[month]
     else:
         return _months[month]
@@ -26,7 +24,12 @@ def find(d1, m1, y1, d2, m2, y2, count=0):
         m1 = m1 + 1
     return find(d1, m1, y1, d2, m2, y2, count)
 
-
 if __name__ == "__main__":
-    print(find(5, 1, 2017, 30, 1, 2017))
-    print(find(26, 8, 1993, 15, 6, 2017))
+    start_time = time.time()
+    print("days:", find(5, 5, 2017, 14, 6, 2017))
+    print("days:", find(23, 4, 2017, 24, 8, 2017))
+    print("days:", find(26, 8, 1993, 11, 1, 2021))
+    print("days:", find(26, 8, 1993, 15, 6, 2017))
+    print("days:", find(23, 4, 2017, 24, 8, 2017))
+    print("days:", find(14, 1, 2016, 28, 4, 2017))
+    print("avg time: {:.6f}".format(time.time() - start_time))
