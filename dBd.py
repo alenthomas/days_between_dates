5a58b0973c602be4ea8a3ac0ba3461299e689264
Alen Thomas <alenthomas9@gmail.com>
2017-06-15 10:07:11 +0530

    return statement if same month and year

--- dBd.py
+++ dBd.py
@@ -1,6 +1,8 @@
 def find(d1, m1, y1, d2, m2, y2, count=0):
     if (d1 == d2 and m1 == m2 and y1 == y2):
         return count
+    if (y1 == y2 and m1 == m2):
+        return find(d1+1, m1, y1, d2, m2, y2, count+1)
 
 if __name__ == "__main__":
-    print(find(1, 1, 2017, 1, 1, 2017))
+    print(find(5, 1, 2017, 30, 2, 2017))
